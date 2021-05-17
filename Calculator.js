dce61bc1390848749b87d05524064d4b694f0195
StevenTabilisma <stabilis@ucsc.edu>
2021-05-16 17:28:05 -0700

    HW 2

---
+++ Calculator.js
@@ -0,0 +1,35 @@
+function add(n1, n2) {
+    var field1 = parseFloat(n1)
+    var field2 = parseFloat(n2)
+    var result = field1+field2;
+    document.getElementById("output").value=result
+    console.log(result)
+}
+
+function subtract(n1, n2) {
+    var field1 = parseFloat(n1)
+    var field2 = parseFloat(n2)
+    var result = field1-field2;
+    document.getElementById("output").value=result
+    console.log(result)
+}
+
+function multiply(n1, n2) {
+    var field1 = parseFloat(n1)
+    var field2 = parseFloat(n2)
+    var result = field1*field2;
+    document.getElementById("output").value=result
+    console.log(result)
+}
+
+function divide(n1, n2) {
+    var field1 = parseFloat(n1)
+    var field2 = parseFloat(n2)
+    var result = field1/field2;
+    document.getElementById("output").value=result
+    console.log(result)
+}
+
+
+
+
